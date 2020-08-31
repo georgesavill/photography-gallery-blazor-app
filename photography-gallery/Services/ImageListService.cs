dc23a4a91e671d3bcec8c9e93390c43c99aea232
georgesavill <gpsavill+github@gmail.com>
2020-08-31 13:55:55 +0100

    Increase image sizes

--- photography-gallery/Services/ImageListService.cs
+++ photography-gallery/Services/ImageListService.cs
@@ -49,7 +49,7 @@ namespace photography_gallery.Services
             Array.Sort(fileList);
             foreach (string entry in fileList)
             {
-                if (entry.IndexOf("_357.") == -1 && entry.IndexOf("_766.") == -1 && entry.IndexOf("_1532.") == -1) // TODO: Make this nicer.
+                if (entry.IndexOf("_400.") == -1 && entry.IndexOf("_800.") == -1 && entry.IndexOf("_1600.") == -1) // TODO: Make this nicer.
                 {
                     returnedFileList.Add(CreateListEntry(entry, "file"));
                 }
@@ -63,9 +63,9 @@ namespace photography_gallery.Services
             string routablePath = relativePath.Split("images/").Last();
             string fileName = relativePath.Split(".").First();
             string fileExtension = relativePath.Split(".").Last();
-            string smallImagePath = fileName + "_357." + fileExtension;
-            string mediumPath = fileName + "_766." + fileExtension;
-            string largePath = fileName + "_1532." + fileExtension;
+            string smallImagePath = fileName + "_400." + fileExtension;
+            string mediumPath = fileName + "_800." + fileExtension;
+            string largePath = fileName + "_1600." + fileExtension;
 
             return new ListEntry(entry, relativePath, routablePath, smallImagePath, mediumPath, largePath, entry.Split("/").Last(), type);
         }
